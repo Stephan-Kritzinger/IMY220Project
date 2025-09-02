93aa9bce732df49dd6b5b4f3789d62df901154b8
stephan-kritzinger <u24581705@tuks.co.za>
2025-09-02 09:10:27 +0200

    Added sign in functionality to the splash page

--- frontend/src/pages/splash.js
+++ frontend/src/pages/splash.js
@@ -4,14 +4,24 @@ import "../styles/splash.css"
 
 const Splash = () => {
     return(
-        <main id="splash">
-            <h1 id="title" class="logo">
-                ProRepo
-            </h1>
-            <p id="slogan" class="logo">
-                The Project Management Site for Professionals
-            </p>
-        </main>
+        <div id="top">
+            <main id="splash">
+                <h1 id="title" className="logo">
+                    ProRepo
+                </h1>
+                <p id="slogan" className="logo">
+                    The Project Management Site for Professionals
+                </p>
+            </main>
+            <section id="login">
+                <div>
+                    <button id="signin">Sign in Now</button>
+                </div>
+                <div>
+                    Dont have an account? <a>Register Now</a>
+                </div>
+            </section>
+        </div>
     )
 };
 
