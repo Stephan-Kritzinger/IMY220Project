3eec68a79aebd953a5314b3db4e9bd06d2856493
stephan-kritzinger <u24581705@tuks.co.za>
2025-09-02 14:55:30 +0200

    Register layout finished, still need to stub api calls, might add additional tabs for profile details later

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -11,8 +11,25 @@ const Register = () => {
                     <Logo />
                     <div className="seperator"></div>
                     <section>
+                        <h2>
+                            Register
+                        </h2>
                         <form>
-
+                            <div className="form-group">
+                                <label for="username">Username</label>
+                                <input name="username" id="username" placeholder="Enter your username" />
+                            </div>
+                            <div className="form-group">
+                                <label for="email">Email Address</label>
+                                <input name="email" id="email" placeholder="example@email.com" />
+                            </div>
+                            <div className="form-group">
+                                <label for="password">Password</label>
+                                <input name="password" id="password" placeholder="At least 8 characters" />
+                            </div>
+                            <div className="form-group">
+                                <button>Register</button>
+                            </div>
                         </form>
                     </section>
                 </div>
