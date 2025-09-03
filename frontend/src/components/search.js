ac2c378b1db0c9186d66c09b83894fbace02cd66
stephan-kritzinger <u24581705@tuks.co.za>
2025-09-03 12:16:05 +0200

    Starting to make search dropdown

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -2,11 +2,24 @@ import React from "react"
 import { Link } from "react-router"
 import "../styles/search.css"
 
+const Result = () => {
+    return(
+        <div className="results">
+            <div className="rHeader">
+                <span className="rActive">Projects</span>
+                <span>People</span>
+            </div>
+            <div className="seperator"></div>
+        </div>
+    )
+}
+
 const Search = () => {
     return(
         <div className="search">
             <input type="search" placeholder="Search for projects/people"></input>
             <i className="fa-solid fa-magnifying-glass"></i>
+            <Result />
         </div>
     )
 }
