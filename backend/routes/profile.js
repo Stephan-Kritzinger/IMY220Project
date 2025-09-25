1714043382ab6a39b28a8ad4b742af4878b5bce1
stephan-kritzinger <u24581705@tuks.co.za>
2025-09-25 15:22:55 +0200

    Added finding mutual friends and removing friends

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -45,10 +45,35 @@ router.get("/", express.json(), async (req, res) => {
 
     //The friends tab will be sanitised to a single field called friendStatus to determine how the friend request button should render.
 
+    const mutuals = currentUser.friends.mutual.filter(user => cursor.friends.mutual.includes(user) && user !== currentUser._id.toString());
+
+    let friendArray = await Promise.all(
+        mutuals.map(async friend => {
+            const person = await user.getByField("_id", ObjectId.createFromHexString(friend));
+            console.log(person)
+            return {
+                username: person.username,
+                picture: person.picture
+            }
+        })
+    );
+
+    const profile = {
+            _id: cursor._id.toString(),
+            username: cursor.username, 
+            email: cursor.email,
+            joindate: cursor. joindate,
+            picture: cursor.picture,
+            details: cursor.details,
+            mutuals: friendArray,
+            repositories: cursor.repositories
+    }
+
+
     return res.status(200).json({
         message: "User retrieved",
         user: {
-            ...cursor
+            ...profile
         }
     })
 })
@@ -148,6 +173,39 @@ router.post("/reject", express.json(), async (req, res) => {
 
     res.status(200).json({
         message: "Friend request rejected"
+    })
+})
+
+router.post("/remove", express.json(), async (req, res) => {
+    const cursor = await user.getByField("_id", ObjectId.createFromHexString(req.body.id));
+    const currentUser = await user.getByField("_id", ObjectId.createFromHexString(req.body.curr_id)); //Logged in user
+
+    if(!cursor){
+        return res.status(404).json({
+            message: "user not found"
+        });
+    }
+
+    //Check if the user was sending a friend request in the first place
+    if(!cursor.friends.mutual?.includes(currentUser._id.toString()) || !currentUser.friends.mutual?.includes(cursor._id.toString())){
+        return res.status(404).json({
+            message: "You werent friends"
+        })
+    }
+
+    await user.update(cursor._id, {
+        $pull: {
+            "friends.mutual": currentUser._id.toString()
+        }
+    });
+    await user.update(currentUser._id, {
+        $pull: {
+            "friends.mutual": cursor._id.toString()
+        }
+    })
+
+    res.status(200).json({
+        message: "Friend removed"
     })
 })
 
